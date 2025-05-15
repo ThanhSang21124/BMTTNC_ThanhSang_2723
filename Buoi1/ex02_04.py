066237ee6ed51e9c04287ea2d989fb17573cf7da
Thanh Sang <thanhsang2112004@gmail.com>
2025-05-15 15:43:57 +0700

    [add] buoi1 ex01 ex02

---
+++ Buoi1/ex02_04.py
@@ -0,0 +1,8 @@
+#tạo một danh sách rỗng để lưu kết quả
+j=[]
+#duyệt qua tất cả các số trong đoạn từ 2000 đến 3200, kiểm tra xem số i có chia hết cho 7 và không phải là bội của 5 không
+for i in range(2000,3201):
+    if(i % 7 == 0) and (i % 5 != 0):
+       j.append(str(i))
+print(','.join(j))
+ 
---
+++ Buoi1/ex02_09.py
@@ -0,0 +1,13 @@
+def kiem_tra_so_nguyen_to(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+# Kiểm tra số nguyên tố và in ra kết quả
+number = int(input("Nhập vào sô cần kiểm tra: "))
+if kiem_tra_so_nguyen_to(number):
+    print(number, "là số nguyên tố.")
+else:
+    print(number, "không phải là số nguyên tố.")
